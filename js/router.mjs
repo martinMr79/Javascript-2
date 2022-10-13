7681875328e63b4344da052f843697b9e0c663e8
Martin Mroz <80599295+martinMr79@users.noreply.github.com>
2022-10-13 14:08:30 +0200

    fixed switch path

--- js/router.mjs
+++ js/router.mjs
@@ -4,19 +4,19 @@ export default function router() {
   const path = location.pathname;
 
   switch (path) {
-    case '/profile/login/':
+    case '/profile/login/index.html':
       listeners.setLoginFormListener()
       return;
-    case '/profile/register/':
+    case '/profile/register/index.html':
       listeners.setRegisterFormListener()
       return;
-    case '/post/create/':
+    case '/post/create/index.html':
       listeners.setCreatePostFormListener()
       return;
-    case '/post/edit/':
+    case '/post/edit/index.html':
       listeners.setUpdatePostListener()
       return;
-    case '/profile/edit/':
+    case '/profile/edit/index.html':
       listeners.setUpdateProfileListener()
       return;
   }
