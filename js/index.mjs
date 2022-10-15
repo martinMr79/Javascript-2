4e74425d56583ae4e0956b86839eaab4a3e3a7be
Martin Mroz <80599295+martinMr79@users.noreply.github.com>
2022-10-15 23:38:39 +0200

    file structure

--- js/index.mjs
+++ js/index.mjs
@@ -1,12 +1,32 @@
-import router from  "./router.mjs";
+//import router from  "./router.mjs";
 
-router();
+import * as listeners from "./handlers/index.mjs"
 
-//import { setRegisterFormListener } from "./handlers/register.mjs";
-//import { setLoginFormListener } from "./handlers/login.mjs";
-//import * as post from "./api/posts/index.mjs";
-//import * as templates from "./templates/index.mjs"
-//import * as postMethods from "./api/posts/index.mjs"
+//router();
+
+import { setRegisterFormListener } from "./handlers/register.mjs";
+import { setLoginFormListener } from "./handlers/login.mjs";
+import * as post from "./api/posts/index.mjs";
+import * as templates from "./templates/index.mjs"
+import * as postMethods from "./api/posts/index.mjs"
+
+const path = location.pathname; 
+
+
+if (path === "/profile/login/index.html") {
+listeners.setLoginFormListener();
+} else if (path === "/profile/register/index.html") {
+listeners.setRegisterFormListener();
+} else if ( path === "/post/create/index.html") {
+ listeners.setCreatePostFormListener()
+} else if (path === "/post/edit/index.html") {
+ listeners.setUpdatePostListener()
+} else if (path ==="/profile/edit/index.html") {
+  listeners.setUpdateProfileListener()
+}
+
+
+
 
 //post.createPost()
 //post.removePost()
---
+++ js/pages/home.mjs
@@ -0,0 +1,44 @@
+import * as postMethods from "./api/posts/index.mjs"
+
+
+export default function router() {
+    const path = location.pathname;
+  
+    switch (path) {
+      case '/profile/login/index.html':
+        listeners.setLoginFormListener()
+        return;
+      case '/profile/register/index.html':
+        listeners.setRegisterFormListener()
+        return;
+      case '/post/create/index.html':
+        listeners.setCreatePostFormListener()
+        return;
+      case '/post/edit/index.html':
+        listeners.setUpdatePostListener()
+        return;
+      case '/profile/edit/index.html':
+        listeners.setUpdateProfileListener()
+        return;
+    }
+  }
+
+
+async function testTemplate() {
+const posts = await postMethods.getPosts()
+const post = posts.pop()
+const container = document.querySelector("#posts")
+   templates.renderPostTemplates(posts, container)
+
+
+
+  }
+
+testTemplate()
+
+post.getPosts().then(console.log)
+
+
+ //  for (let i = 0; i < Posts.length; i++) {
+ //   posts.innerHTML += `<P>fdfdfdf</p>`
+ //  }
