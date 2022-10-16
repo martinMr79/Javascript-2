aa1d6536aeadd6a469b63e49137d6b5bdc7e5a63
Martin Mroz <80599295+martinMr79@users.noreply.github.com>
2022-10-16 19:29:54 +0200

    create posts & posts working

--- js/index.mjs
+++ js/index.mjs
@@ -1,36 +1,48 @@
 //import router from  "./router.mjs";
 
-import * as listeners from "./handlers/index.mjs"
+import * as listeners from "./handlers/index.mjs";
 
 //router();
 
 import { setRegisterFormListener } from "./handlers/register.mjs";
 import { setLoginFormListener } from "./handlers/login.mjs";
 import * as post from "./api/posts/index.mjs";
-import * as templates from "./templates/index.mjs"
-import * as postMethods from "./api/posts/index.mjs"
+import * as templates from "./templates/index.mjs";
+import * as postMethods from "./api/posts/index.mjs";
 
-const path = location.pathname; 
+const path = location.pathname;
 
 
 if (path === "/profile/login/index.html") {
-listeners.setLoginFormListener();
-} else if (path === "/profile/register/index.html") {
-listeners.setRegisterFormListener();
-} else if ( path === "/post/create/index.html") {
- listeners.setCreatePostFormListener()
-} else if (path === "/post/edit/index.html") {
- listeners.setUpdatePostListener()
-} else if (path ==="/profile/edit/index.html") {
-  listeners.setUpdateProfileListener()
+  listeners.setLoginFormListener();
+} 
+else if (path === "/profile/register/index.html") {
+  listeners.setRegisterFormListener();
+} 
+//else if (path === "/post/create/index.html") {
+//  listeners.setCreatePostFormListener();
+//} 
+else if (path === "/post/edit/index.html") {
+  listeners.setUpdatePostListener();
+} 
+else if (path === "/profile/edit/index.html") {
+  listeners.setUpdateProfileListener();
+} 
+else if (path === "/posts/index.html") {
+  listeners.setCreatePostFormListener();
+  displayPosts();
+  post.createPost()
+}
+
+async function displayPosts() {
+  const posts = await postMethods.getPosts();
+  //const post = posts.pop();
+  const container = document.querySelector("#posts");
+  templates.renderPostTemplates(posts, container);
 }
 
 
-
-
-//post.createPost()
 //post.removePost()
 //post.updatePost()
 //post.getPost()
-//post.getPosts().then(console.log)
-
+// post.getPosts().then(console.log);
--- js/pages/home.mjs
+++ js/pages/home.mjs
@@ -1,27 +1,7 @@
 import * as listeners from "./api/posts/index.mjs"
 
 
-function router() {
-    const path = location.pathname;
-  
-    switch (path) {
-      case '/profile/login/index.html':
-        listeners.setLoginFormListener()
-        return;
-      case '/profile/register/index.html':
-        listeners.setRegisterFormListener()
-        return;
-      case '/post/create/index.html':
-        listeners.setCreatePostFormListener()
-        return;
-      case '/post/edit/index.html':
-        listeners.setUpdatePostListener()
-        return;
-      case '/profile/edit/index.html':
-        listeners.setUpdateProfileListener()
-        return;
-    }
-  }
+
 
 
 async function testTemplate() {
@@ -38,7 +18,3 @@ testTemplate()
 
 post.getPosts().then(console.log)
 
-
- //  for (let i = 0; i < Posts.length; i++) {
- //   posts.innerHTML += `<P>fdfdfdf</p>`
- //  }
